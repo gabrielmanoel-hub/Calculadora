49ebe9a6ed1029fc04ee0a1c7562ebf215c02dc1
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2023-02-22 21:07:23 -0300

    first commit

--- index.js
+++ index.js
@@ -21,48 +21,53 @@ const operator = {
 }
 
 
-const bnt = document.querySelectorAll('button')
-bnt.forEach((element) => {
+const getElement = document.querySelectorAll('button')
+getElement.forEach((element) => {
    element.addEventListener('click',(element) => {
-      allClear(element)
-
-      if(element && checkRepeats(element.target.id)) return
-
-      textOutput(element)
-      if(element.target.id === '=') {
-         result.innerHTML = calculation(result, operator);
-      }
       
+      sendElementToScreen(element)
       clear(element)
+      cleanAll(element)
+      limitRepeats(element)
+      calculus(element)
+      newCalculation(element)
    })
 })
 
-function textOutput(element) {
-   if(/[^=AC]/g.test(element.target.id)) {
+const sendElementToScreen = (element) => {
+   if(element.target.id.match(/[.\d]/g)) {
       result.innerHTML += element.target.id
+   } 
+}
+
+const limitRepeats = (element) => {
+   if(element.target.id.match(/[^\dAC.=]/g)) {
+       result.innerHTML += element.target.id
+   } 
+
+}
+
+const clear = (element) => {
+   if(element.target.id === 'C') {
+      result.innerHTML = result.innerHTML.slice(0, -1)
    }
 }
 
-
-
-function allClear(element) {
+const cleanAll = (element) => {
    if(element.target.id === 'AC') {
-      result.innerText = ''
+      result.innerHTML = ''
    }
 }
 
-function clear(element) {
-   if(element.target.id === 'C') result.innerText = result.innerText.slice(0, -1)
+const calculus = (element) => {
+   if(element.target.id === '=') {
+      result.innerHTML = eval(result.innerHTML.toString())
+      console.log(result.innerHTML.split(/[.\d]/g).join())
+   }
 }
 
-
-function checkRepeats(element) {
-   let operadores = Object.values(operator)
-   let boolean = null
-      operadores.forEach((e) => {
-         if(result.innerText.includes(e) && element === e) {
-            boolean = true
-         }
-      })
-      return boolean
+const newCalculation = (element) => {
+   if(element.target.id === '=' && result.innerHTML.match(/\d/g)) {
+      result.innerHTML = ''
+   }
 }
